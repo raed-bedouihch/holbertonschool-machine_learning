4c3b9ef00793b953f390dcae866ace67ae5a1f8d
raed <raed.da3k@gmail.com>
2024-04-19 09:56:45 +0100

    add

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""10. I’ll Use My Scale"""
+
+
+def np_shape(matrix):
+    """
+      Function that calculates the shape of a numpy.ndarray
+      """
+    return matrix.shape
