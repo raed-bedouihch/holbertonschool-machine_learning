57e1a6ee5736fb18178a207fd8ed7464cf7e8109
raed <raed.da3k@gmail.com>
2024-04-19 09:57:51 +0100

    add

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""12. Bracing The Elements"""
+
+
+def np_elementwise(mat1, mat2):
+    """performs element-wise addition, subtraction,
+    multiplication, and division
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
