7c0d76256449383e3c3be835feb44d20cb2c41b5
raed <raed.da3k@gmail.com>
2024-04-24 12:53:30 +0100

    add

---
+++ math/plotting/3-two.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""3. Two is better than one"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """
+    plot x ↦ y1 and x ↦ y2 as line graphs
+    """
+
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.xlabel('Time (years)')
+    plt.xlim(0, 20000)
+    plt.ylabel('Fraction Remaining')
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.show()
