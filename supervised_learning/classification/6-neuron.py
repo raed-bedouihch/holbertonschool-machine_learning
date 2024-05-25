4389d80e2adfd690ec787c78677a2086248d828e
raed <raed.da3k@gmail.com>
2024-05-25 11:09:52 +0100

    add

---
+++ supervised_learning/classification/6-neuron.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+""" 1. Neuron """
+import numpy as np
+
+
+class Neuron:
+    """ Class that defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """ Class constructor for Neuron """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Returns the values of the weights vector """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Returns the value of the bias neuron """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Returns the value of the activation function output """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neuron """
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ Calculates the cost of the model using logistic regression """
+        m = Y.shape[1]
+        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neuron’s predictions """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neuron """
+        m = Y.shape[1]
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / m
+        db = np.sum(dz) / m
+        self.__W -= (alpha * dw).T
+        self.__b -= alpha * db
+        return self.__W, self.__b
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Trains the neuron """
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            self.__A = self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha)
+        return self.evaluate(X, Y)
