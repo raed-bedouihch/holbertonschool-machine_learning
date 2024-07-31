6542c50fe5ef1e88471cd26046f85e93240ece6c
raed <raed.da3k@gmail.com>
2024-07-31 12:05:47 +0100

    forward propagation

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 2. Forward Propagation """
+
+
+import tensorflow.compat.v1 as tf
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """ creates the forward propagation graph for the neural network
+    """
+    layer = create_layer(x, layer_sizes[0], activations[0])
+    for i in range(1, len(layer_sizes)):
+        layer = create_layer(layer, layer_sizes[i], activations[i])
+
+    return layer
