42d5152fe845d09634d83bec610d115ad2d76e9b
raed <raed.da3k@gmail.com>
2024-08-01 10:28:26 +0100

    train process

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 6. Train """
+
+
+import tensorflow.compat.v1 as tf
+
+
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
