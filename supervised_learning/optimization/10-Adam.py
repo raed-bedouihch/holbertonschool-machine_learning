05eccf4a7b73b383c966dc6d4e8ede31519cbafb
raed <raed.da3k@gmail.com>
2024-08-08 15:22:33 +0100

    add

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 10. Adam Upgraded """
+
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """ sets up the Adam optimization algorithm in TensorFlow """
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
+
+    return optimizer
