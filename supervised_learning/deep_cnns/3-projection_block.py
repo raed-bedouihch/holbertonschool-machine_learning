629eae790c0f818f41734c01031b7dd2595d5f3f
raed <raed.da3k@gmail.com>
2024-08-17 21:28:15 +0100

    add

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python3
-""" 3. Pooling Back Prop """
+"""
+building the core block of the
+residual neural network(projection not residual)
+"""
+
+import tensorflow.keras as K
 
 
-import numpy as np
-
-
-def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """ that performs back propagation over
-    a pooling layer of a neural network
+def projection_block(A_prev, filters, s=2):
     """
-
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw = kernel_shape
-    sh, sw = stride
-    m, h_new, w_new, c_new = dA.shape
-    dA_prev = np.zeros(A_prev.shape)
-
-    for i in range(m):
-        a_prev = A_prev[i]
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    v_start = h * sh
-                    v_end = v_start + kh
-                    h_start = w * sw
-                    h_end = h_start + kw
-                    if mode == 'max':
-                        a_slice = a_prev[v_start:v_end, h_start:h_end, c]
-                        mask = (a_slice == np.max(a_slice))
-                        dA_prev[i, v_start:v_end, h_start:h_end, c] +=\
-                            np.multiply(mask, dA[i, h, w, c])
-                    else:
-                        da = dA[i, h, w, c]
-                        shape = kernel_shape
-                        avg = da / (kh * kw)
-                        Z = np.ones(shape) * avg
-                        dA_prev[i, v_start:v_end, h_start:h_end, c] += Z
-
-    return dA_prev
+    explanation later goes here
+    """
+    shortcut = K.layers.Conv2D(filters[2], kernel_size=(1, 1),
+                               strides=(s, s), padding='same',
+                               kernel_initializer=
+                               K.initializers.he_normal(seed=
+                                                        None))(A_prev)
+    shortcut_bn = K.layers.BatchNormalization()(shortcut)
+    first_conv = K.layers.Conv2D(filters[0], kernel_size=(1, 1),
+                                 strides=(s, s), padding='same',
+                                 kernel_initializer=
+                                 K.initializers.he_normal(seed=
+                                                          None))(A_prev)
+    first_bn = K.layers.BatchNormalization()(first_conv)
+    activation1 = K.layers.Activation("relu")(first_bn)
+    second_conv = K.layers.Conv2D(filters[1], kernel_size=(3, 3),
+                                  strides=(1, 1), padding='same',
+                                  kernel_initializer=
+                                  K.initializers.he_normal(seed=
+                                                           None))(activation1)
+    second_bn = K.layers.BatchNormalization()(second_conv)
+    activation2 = K.layers.Activation("relu")(second_bn)
+    third_conv = K.layers.Conv2D(filters[2], kernel_size=(1, 1),
+                                 strides=(1, 1), padding='same',
+                                 kernel_initializer=K.initializers.he_normal(seed=None))(activation2)
+    third_bn = K.layers.BatchNormalization()(third_conv)
+    activation3 = K.layers.Activation("relu")(third_bn)
+    activated_output = K.layers.Add()([shortcut_bn, activation3])
+    return activated_output
