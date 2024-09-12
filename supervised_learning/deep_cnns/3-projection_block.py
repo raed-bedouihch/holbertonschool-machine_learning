e01bf858faa01045cfd0fb46ff76dfb003ed5e7b
raed <raed.da3k@gmail.com>
2024-09-12 12:10:48 +0100

    update

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
-"""
-building the core block of the
-residual neural network(projection not residual)
-"""
+""" 3. Projection Block """
 
-import tensorflow as K
+
+from tensorflow import keras as K
 
 
 def projection_block(A_prev, filters, s=2):
-    """
-    explanation later goes here
-    """
-    shortcut = K.layers.Conv2D(filters[2], kernel_size=(1, 1),
-                               strides=(s, s), padding='same',
-                               kernel_initializer=
-                               K.initializers.he_normal(seed=
-                                                        None))(A_prev)
-    shortcut_bn = K.layers.BatchNormalization()(shortcut)
-    first_conv = K.layers.Conv2D(filters[0], kernel_size=(1, 1),
-                                 strides=(s, s), padding='same',
-                                 kernel_initializer=
-                                 K.initializers.he_normal(seed=
-                                                          None))(A_prev)
-    first_bn = K.layers.BatchNormalization()(first_conv)
-    activation1 = K.layers.Activation("relu")(first_bn)
-    second_conv = K.layers.Conv2D(filters[1], kernel_size=(3, 3),
-                                  strides=(1, 1), padding='same',
-                                  kernel_initializer=
-                                  K.initializers.he_normal(seed=
-                                                           None))(activation1)
-    second_bn = K.layers.BatchNormalization()(second_conv)
-    activation2 = K.layers.Activation("relu")(second_bn)
-    third_conv = K.layers.Conv2D(filters[2], kernel_size=(1, 1),
-                                 strides=(1, 1), padding='same',
-                                 kernel_initializer=K.initializers.he_normal(seed=None))(activation2)
-    third_bn = K.layers.BatchNormalization()(third_conv)
-    activation3 = K.layers.Activation("relu")(third_bn)
-    activated_output = K.layers.Add()([shortcut_bn, activation3])
-    return activated_output
+    """ builds a projection block """
+
+    init = K.initializers.he_normal(seed=0)
+    F11, F3, F12 = filters
+
+    c_F11 = K.layers.Conv2D(F11, kernel_size=1, padding="same", strides=s,
+                            kernel_initializer=init)(A_prev)
+    norm_F11 = K.layers.BatchNormalization()(c_F11)
+    act_F11 = K.layers.Activation("relu")(norm_F11)
+
+    c_F3 = K.layers.Conv2D(F3, kernel_size=3, padding="same", strides=1,
+                           kernel_initializer=init)(act_F11)
+    norm_F3 = K.layers.BatchNormalization()(c_F3)
+    act_F3 = K.layers.Activation("relu")(norm_F3)
+    c_F12 = K.layers.Conv2D(F12, kernel_size=1, padding="same", strides=1,
+                            kernel_initializer=init)(act_F3)
+    norm_F12 = K.layers.BatchNormalization()(c_F12)
+
+    shortcut = K.layers.Conv2D(F12, kernel_size=1, padding="same", strides=s,
+                               kernel_initializer=init)(A_prev)
+    shortcut = K.layers.BatchNormalization()(shortcut)
+
+    X = K.layers.Add()([norm_F12, shortcut])
+
+    output = K.layers.Activation("relu")(X)
+
+    return output
