d6340ccee5d3b788f2a8cdf5f582effa6dcf73a4
raed <raed.da3k@gmail.com>
2024-09-21 10:27:29 +0100

    add

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-"""10. Initialize Binomial"""
-class Binomial():
-    """represents a binomial distribution"""
+"""
+This is our main binomial class
+"""
+
+
+class Binomial:
+    """
+    this defines the binomial class to
+    understand the core concepts of
+    binomial distribution
+    """
     def __init__(self, data=None, n=1, p=0.5):
+        """
+        init function to check values and calculate a reasonable value for
+        the number of trials and probability of success
+        """
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
-            self.n = int(n)
-            if 0 >= p or p >= 1:
+            if not (0 < p < 1):
                 raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(round(n))
             self.p = float(p)
         else:
-            if type(data) is not list:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(data) <= 1:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            mean = sum(data) / len(data)
-            variance = sum((x - mean)**2 for x in data) / (len(data))
-            self.n = round(mean / (1 - (variance / mean)))
-            self.p = float(mean / self.n)
+
+            p = sum(data) / (len(data) * self.n)
+            self.n = int(round(len(data) / (1 - p)))
+            self.p = sum(data) / (len(data) * self.n)
