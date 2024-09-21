c843e54501a6a566f0f7a059698507770e5683ea
raed <raed.da3k@gmail.com>
2024-09-21 10:13:04 +0100

    add

---
+++ math/probability/exponential.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 3. Initialize Exponential """
+class Exponential:
+    """represents an exponential distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        """constructor function"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float((1 / sum(data)) / (1 / len(data)))
