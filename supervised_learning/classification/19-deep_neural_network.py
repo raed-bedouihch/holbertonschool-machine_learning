ca08e99ee12f44dca9a980a1ad2614b341de5459
raed <raed.da3k@gmail.com>
2024-09-22 14:00:33 +0100

    add

--- supervised_learning/classification/19-deep_neural_network.py
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -4,45 +4,49 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    """ defines a deep neural network performing binary classification """
+    """Defines a deep neural network performing binary classification."""
 
     def __init__(self, nx, layers):
-        """constructor for DeepNeuralNetwork"""
+        """Constructor for DeepNeuralNetwork."""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        for layer in range(self.L):
+
+        for layer in range(self.__L):
             if not isinstance(layers[layer], int) or layers[layer] < 1:
                 raise TypeError("layers must be a list of positive integers")
             if layer == 0:
-                he_initialized_weights = np.random.randn(
-                    layers[layer], nx) * np.sqrt(2 / nx)
+                self.__weights['W' + str(layer + 1)] = (
+                    np.random.randn(layers[layer], nx) *
+                    np.sqrt(2 / nx)
+                )
             else:
-                he_initialized_weights = np.random.randn(
-                    layers[layer], layers[layer - 1]
-                ) * np.sqrt(2 / layers[layer - 1])
-            self.__weights['W' + str(layer + 1)] = he_initialized_weights
+                self.__weights['W' + str(layer + 1)] = (
+                    np.random.randn(layers[layer], layers[layer - 1]) *
+                    np.sqrt(2 / layers[layer - 1])
+                )
             self.__weights['b' + str(layer + 1)] = np.zeros((layers[layer], 1))
 
     @property
     def L(self):
-        """getter for L"""
+        """Getter for L."""
         return self.__L
 
     @property
     def cache(self):
-        """getter for cache"""
+        """Getter for cache."""
         return self.__cache
 
     @property
     def weights(self):
-        """getter for weights"""
+        """Getter for weights."""
         return self.__weights
 
     def forward_prop(self, X):
@@ -50,7 +54,7 @@ class DeepNeuralNetwork:
         self.__cache['A0'] = X  # Save input data in cache
         A = X  # Start with the input as the output for the first layer
 
-        for layer in range(1, self.L + 1):
+        for layer in range(1, self.__L + 1):
             W = self.__weights['W' + str(layer)]
             b = self.__weights['b' + str(layer)]
 
@@ -68,9 +72,6 @@ class DeepNeuralNetwork:
     def cost(self, Y, A):
         """Calculates the cost of the model using logistic regression."""
         m = Y.shape[1]  # Number of examples
-
-        # Calculate the cost
-        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
-        cost = -np.sum(cost) / m  # Average cost
-
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * 
+                       np.log(1.0000001 - A)) / m
         return cost
