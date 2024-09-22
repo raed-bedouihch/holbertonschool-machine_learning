d264a6cb68600782111af568e557431d2055783a
raed <raed.da3k@gmail.com>
2024-09-22 13:46:02 +0100

    add

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -1,81 +1,66 @@
 #!/usr/bin/env python3
-"""
-Creating the deep neural network
-by my own
-"""
-
-
+"""DeepNeuralNetwork"""
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """
-    this is a deep neural network
-    performing binary classification
-    """
-    __L = None
-    __cache = None
-    __weights = None
+    """ defines a deep neural network performing binary classification """
 
     def __init__(self, nx, layers):
-        """
-        initialisation function to get the number
-        of layers to initialize the cache and
-        initliaze the weights and biases
-        """
+        """constructor for DeepNeuralNetwork"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or layers == []:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
         self.__L = len(layers)
-        self.__cache = dict()
-        self.__weights = dict()
-        for i in range(len(layers)):
-            if type(layers[i]) != int or layers[i] <= 0:
+        self.__cache = {}
+        self.__weights = {}
+        for layer in range(self.L):
+            if not isinstance(layers[layer], int) or layers[layer] < 1:
                 raise TypeError("layers must be a list of positive integers")
-            if i == 0:
-                He = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
-                self.__weights["W" + str(i + 1)] = He
+            if layer == 0:
+                he_initialized_weights = np.random.randn(
+                    layers[layer], nx) * np.sqrt(2 / nx)
             else:
-                He = np.random.randn(
-                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-                self.__weights['W' + str(i + 1)] = He
-            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+                he_initialized_weights = np.random.randn(
+                    layers[layer], layers[layer - 1]
+                ) * np.sqrt(2 / layers[layer - 1])
+            self.__weights['W' + str(layer + 1)] = he_initialized_weights
+            self.__weights['b' + str(layer + 1)] = np.zeros((layers[layer], 1))
 
     @property
     def L(self):
-        """
-        layers number getter
-        """
+        """getter for L"""
         return self.__L
 
     @property
     def cache(self):
-        """
-        dnn cache getter
-        """
+        """getter for cache"""
         return self.__cache
 
     @property
     def weights(self):
-        """
-        weights getter
-        """
+        """getter for weights"""
         return self.__weights
 
     def forward_prop(self, X):
-        """
-        applying forward propagation in a layer
-        using the sigmoid function and all the weights
-        """
-        self.cache['A0'] = X
-        for l in range(1, self.L + 1):
-            W = self.weights['W' + str(l)]
-            b = self.weights['b' + str(l)]
-            Z = np.dot(W, self.cache['A' + str(l - 1)]) + b
+        """Calculates the forward propagation of the neural network."""
+        self.__cache['A0'] = X  # Save input data in cache
+        A = X  # Start with the input as the output for the first layer
+
+        for layer in range(1, self.L + 1):
+            W = self.__weights['W' + str(layer)]
+            b = self.__weights['b' + str(layer)]
+
+            # Calculate Z for the current layer
+            Z = np.dot(W, A) + b
+
+            # Apply the sigmoid activation function
             A = 1 / (1 + np.exp(-Z))
-            self.cache['A' + str(l)] = A
-        return A, self.cache
+
+            # Save the activated output in the cache
+            self.__cache['A' + str(layer)] = A
+
+        return A, self.__cache
