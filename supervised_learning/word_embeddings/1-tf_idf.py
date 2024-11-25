275abad093f4118b457adade77b3e424f86e2e0e
raed <raed.da3k@gmail.com>
2024-11-25 18:17:10 +0100

    add

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""tf-idf"""
-import numpy as np
+"""a function that creates a TF-IDF embedding"""
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    """TF-IDF"""
-    vectorizer = TfidfVectorizer(vocabulary=vocab)
-    X = vectorizer.fit_transform(sentences)
-    return X.toarray(), vectorizer.get_feature_names()
+    """sentences is a list of sentences to analyze
+    vocab is a list of the vocabulary words to use for the analysis"""
+    tf_idf_vect = TfidfVectorizer(vocabulary=vocab)
+    tf_idf = tf_idf_vect.fit_transform(sentences).toarray()
+    features = tf_idf_vect.get_feature_names_out()
+    return tf_idf, features
