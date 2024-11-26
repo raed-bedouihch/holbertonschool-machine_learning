bc556ce9f060e9a61bae22a7715a5c02bbb5db42
raed <raed.da3k@gmail.com>
2024-11-26 19:28:58 +0100

    update

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python3
-""" 6. Expectation """
+"""a function that calculates the probability density
+function of a Gaussian distribution"""
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """ Calculates the expectation step in the EM algorithm for a GMM """
-
-    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
-            not isinstance(pi, np.ndarray) or pi.ndim != 1 or
-            not isinstance(m, np.ndarray) or m.ndim != 2 or
-            not isinstance(S, np.ndarray) or S.ndim != 3 or
-            X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0] or
-            S.shape[1] != S.shape[2] or
-            pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]):
+    """calculates the expectation step in the EM algorithm for a GMM"""
+    try:
+        n, d = X.shape
+        if not np.isclose(np.sum(pi), 1):
+            return None, None
+        k = len(pi)
+        g = np.zeros((k, n))
+        for i in range(k):
+            g[i] = pi[i] * pdf(X, m[i], S[i])
+        g_sum = np.sum(g, axis=0)
+        g /= g_sum
+        tot_l = np.sum(np.log(g_sum))
+        return g, tot_l
+    except BaseException:
         return None, None
-
-    if not np.isclose([np.sum(pi)], [1])[0]:
-        return None, None
-
-    k = pi.shape[0]
-
-    pdfs = np.array([pdf(X, m[i], S[i]) for i in range(k)])
-
-    weighted_pdfs = pi[:, np.newaxis] * pdfs
-
-    marginal_prob = np.sum(weighted_pdfs, axis=0)
-    post_probs = weighted_pdfs / marginal_prob
-
-    log_likelihood = np.sum(np.log(marginal_prob))
-
-    return post_probs, log_likelihood
