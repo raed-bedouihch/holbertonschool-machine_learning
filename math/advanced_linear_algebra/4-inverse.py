edcd0eab3f64b5ae7fe4047d2e3330048b67d4d0
raed <raed.da3k@gmail.com>
2024-11-26 19:14:56 +0100

    inverse

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
-""" 4. Inverse """
+"""a function that calculates the inverse of a matrix"""
+
 adjugate = __import__('3-adjugate').adjugate
 determinant = __import__('0-determinant').determinant
 
 
 def inverse(matrix):
-    """ calculates the inverse of a matrix """
-    adjugate_matrix = adjugate(matrix)
-    det_value = determinant(matrix)
+    """Returns: the inverse of matrix or None if matrix is singular"""
 
-    if det_value == 0:
+    if not isinstance(matrix, list) or not all(
+        isinstance(
+            row,
+            list) for row in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    n_rows = len(matrix)
+    if any(len(row) != n_rows for row in matrix) or n_rows == 0:
+        raise ValueError('matrix must be a non-empty square matrix')
+    adj_matrix = adjugate(matrix)
+    n_rows = len(adj_matrix)
+    det = determinant(matrix)
+    if det == 0:
         return None
-    inverse_matrix = []
-
-    for i in range(len(adjugate_matrix)):
-        row = []
-        for j in range(len(adjugate_matrix[0])):
-            row.append(adjugate_matrix[i][j] / det_value)
-        inverse_matrix.append(row)
-    return inverse_matrix
+    for row in range(n_rows):
+        for col in range(n_rows):
+            adj_matrix[row][col] /= det
+    return adj_matrix
