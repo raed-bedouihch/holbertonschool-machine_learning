1a1a6c2bbee95c908de57f3fb56dee50a90b28d3
raed <raed.da3k@gmail.com>
2024-12-08 18:35:58 +0100

    add

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a function that takes a pd.DataFrame as input and
+select the last 10 rows of the High and Close columns
+then Convert these selected values into a numpy.ndarray."""
+
+
+def array(df):
+    """Returns: the numpy.ndarray"""
+    return (df[["High", "Close"]].iloc[-10:]).to_numpy()
